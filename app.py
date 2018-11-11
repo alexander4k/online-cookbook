8a39e8d1339d3473f879281a0ef00012ff42021a
Alexander Kaczmarek <k.alexander@live.com>
2018-11-11 20:43:58 +0000

    Added .gitignore file and connected to the database

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
+from flask_pymongo import PyMongo
+
+import db_connection
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = db_connection.dbname
+app.config["MONGO_URI"] = db_connection.uri
+
+mongo = PyMongo(app)
+
 @app.route("/")
 def hello():
-    return "hello"
+    recipes = mongo.db.recipes.find()
+    
+    return render_template("home.html", recipes=recipes)
     
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
