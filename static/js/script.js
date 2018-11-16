139bfb522400b457cb29a34e7ad3e7e1711597fc
Alexander Kaczmarek <k.alexander@live.com>
2018-11-16 21:52:38 +0000

    Added functionality for carousel arrows and adding/removing new add ingredients/instructions fields on the add_recipe.html page, removed the function responsible for changing cursor to grabbing while over the carousel

--- static/js/script.js
+++ static/js/script.js
@@ -2,30 +2,19 @@ $(document).ready(function() {
     $(".recent").hide()
     $(".least_popular").hide()
     $('.sidenav').sidenav();
-    
+    $('.tooltipped').tooltip();
     refreshSlide()
     refreshSelect()
-    
-    slideCursorToGrab()
     changeLikeIconAndCursor()
+    addAndRemoveElements()
+    carouselArrows()
+    carouselOptionsBehaviour(1)
+    carouselOptionsBehaviour(2)
+    carouselOptionsBehaviour(3)
     
-    slideOptionsBehaviour(1)
-    slideOptionsBehaviour(2)
-    slideOptionsBehaviour(3)
 });
 
-function slideCursorToGrab(){
-    $(".recipe_slide").on({
-        mousedown: function() {
-            $(this).css("cursor", "grabbing");
-        },
-        mouseup: function() {
-            $(this).css("cursor", "grab");
-        }
-    });
-}
-
-function changeLikeIconAndCursor(){
+function changeLikeIconAndCursor() {
     $(".fa-star").on({
         mousedown: function() {
             $(this).css("cursor", "pointer");
@@ -66,7 +55,7 @@ function showAndHideRest(show_option) {
     $(show_option).show()
 }
 
-function slideOptionsBehaviour(num_of_option) {
+function carouselOptionsBehaviour(num_of_option) {
     $(".option" + String(num_of_option)).click(function() {
         if (!$(this).hasClass("active")) {
             if (num_of_option == 1) {
@@ -109,5 +98,37 @@ function slideOptionsBehaviour(num_of_option) {
             $(".option3").addClass("active")
         }
         refreshSlide()
+    })
+}
+
+function carouselArrows(){
+    $('.moveNextCarousel').click(function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        $('.carousel').carousel('next');
+    });
+
+    $('.movePrevCarousel').click(function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        $('.carousel').carousel('prev');
+    });
+}
+
+function addAndRemoveElements() {
+    $(".add_ingredient").click(function() {
+        $(".ingredients").append('<div class="input-field col s12 m11 offset-m1 another"><textarea name="instruction" class="materialize-textarea"></textarea></div>');
+    });
+
+    $(".remove_ingredient").click(function() {
+        $(".ingredients .another:last-child").last().remove()
+    })
+
+    $(".add_instruction").click(function() {
+        $(".instructions").append('<div class="input-field col s12 m11 offset-m1 another"><textarea name="ingredient" class="materialize-textarea"></textarea></div>');
+    });
+
+    $(".remove_instruction").click(function() {
+        $(".instructions .another:last-child").last().remove()
     })
 }
