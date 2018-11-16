ec395391963d8352ebb97fa2e5e870d2b0fe0b82
Alexander Kaczmarek <k.alexander@live.com>
2018-11-16 21:49:48 +0000

    Added functions for converting items in a list to lowercase and capitalized

---
+++ utils/misc.py
@@ -0,0 +1,13 @@
+def convert_items_in_list_to_lower(list_to_convert):
+    new_list = []
+    for item in list_to_convert:
+        new_list.append(item.lower())
+        
+    return new_list
+    
+def convert_items_in_list_to_capitalized(list_to_convert):
+    new_list = []
+    for item in list_to_convert:
+        new_list.append(item.capitalize())
+        
+    return new_list
