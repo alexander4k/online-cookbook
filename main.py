66a38917f4472456a30515e1a9fef79f2f472f13
Alexander Kaczmarek <k.alexander@live.com>
2018-11-17 17:43:54 +0000

    Added route/view for recipe_details.html

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import time
 from flask import Flask, render_template, request, redirect, url_for
+from bson.objectid import ObjectId
 
 from utils import app_setup, queries, misc
 
@@ -13,7 +14,7 @@ def home():
     most_popular = queries.get_top_ten_descending("votes")
     least_popular = queries.get_top_ten_ascending("votes")
     
-    formatted_dates = queries.format_dates()
+    formatted_dates = queries.format_dates(queries.get_top_ten_descending("creation_date"))
     
     return render_template("home.html",
                             most_popular=most_popular,
@@ -58,7 +59,28 @@ def insert_recipe():
                            filter(None, request.form.getlist("ingredient")),
                            filter(None, request.form.getlist("instruction")))
                            
-    return redirect(url_for("add_recipe"))                   
+    return redirect(url_for("add_recipe")) 
+    
+@app.route("/recipe_details/<recipe_id>")
+def recipe_details(recipe_id):
+    recipe = app_setup.mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
+    
+    converted_creation_date = queries.format_date(recipe["creation_date"])
+    time = recipe["time"]
+    nutrition_values = queries.list_values(recipe["nutritional"])
+    nutrition_names = sorted(misc.convert_items_in_list_to_capitalized(recipe["nutritional"]))
+    return render_template("recipe_details.html",
+                            recipe=recipe,
+                            creation_date=converted_creation_date,
+                            time=time,
+                            nutrition_values=nutrition_values,
+                            nutrition_names=nutrition_names)
+                            
+@app.route("/add_vote/<recipe_id>")
+def add_vote(recipe_id):
+    app_setup.mongo.db.recipes.update_one({"_id" : ObjectId(recipe_id)}, {"$inc": { "votes": 1}})
+    
+    return redirect(url_for("recipe_details", recipe_id=recipe_id))
     
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
