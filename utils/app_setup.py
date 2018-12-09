1ee95a6c19bdabd3be49bd8b0aef3b459c374ff5
Alexander Kaczmarek <k.alexander@live.com>
2018-12-09 04:38:59 +0000

    Hid environment variables

--- utils/app_setup.py
+++ utils/app_setup.py
@@ -1,10 +1,22 @@
+import os
 from flask import Flask
 from flask_pymongo import PyMongo
+from dotenv import load_dotenv
+
+"""
+Setup the app
+"""
+
+# Read from .env file and add to environment variable
+load_dotenv(dotenv_path="secret_settings/.env")
 
 app = Flask("main")
     
-app.config["MONGO_DBNAME"] = "online-cookbook"
-app.config["MONGO_URI"] = "mongodb://admin:kawaii1010@ds259463.mlab.com:59463/online-cookbook"
+app.config["MONGO_DBNAME"] = os.getenv("DBNAME", 'defualt_dbname')
+app.config["MONGO_URI"] = os.getenv("URI", 'default_uri')
+
+SECRET_KEY = os.getenv("SECRET_KEY", '123456789')
+app.secret_key = SECRET_KEY.encode("utf-8")
     
 mongo = PyMongo(app)
     
