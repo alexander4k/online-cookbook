601f3541a9075f4cf5d3ee981f0ad77ebc592a3a
Alexander Kaczmarek <k.alexander@live.com>
2018-12-09 04:51:36 +0000

    Added comment to the check_if_user_in_session function

--- utils/sessions.py
+++ utils/sessions.py
@@ -1,6 +1,10 @@
 from flask import session
 
 def check_if_user_in_session():
+    """
+    Check if user is in session and if yes,
+    return the user's username
+    """
     username = None
     if "user" in session:
         username = session["user"]
