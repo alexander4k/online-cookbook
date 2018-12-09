e4947f054381c58e9353a10c4f78d97c1d468949
Alexander Kaczmarek <k.alexander@live.com>
2018-12-09 04:50:57 +0000

    Removed unused functions. Added a function for assigning incoming request data to a variable. Added a function for setting query conditions based on filter options chosen by user. Added a function for setting query sort condition based on option selected by user. Added a function to find recipes according to filter and sort options selected by a user. Added functions to find category and cuisine name in their respective collections using their ids and a function to return allergen name. Added comments

--- utils/queries.py
+++ utils/queries.py
@@ -5,29 +5,26 @@ from bson.objectid import ObjectId
 from utils import app_setup, misc
 
 
-def get_top_ten_descending(field):
-    return app_setup.mongo.db.recipes.find().sort(field, pymongo.DESCENDING).limit(10)
+def insert_user(username, password):
+    """
+    Creates a user object and inserts it into the database
+    """
     
-def get_top_ten_ascending(field):
-    return app_setup.mongo.db.recipes.find().sort(field, pymongo.ASCENDING).limit(10)
+    user = {
+        "username": username,
+        "password": password,
+        "favourite_recipes": []
+    }
     
-def format_dates(data):
-    recipes = data
-    formatted_dates = []
-        
-    for recipe in recipes:
-        formatted_dates.append(time.strftime("%d-%m-%Y-%H-%M-%S", time.gmtime(recipe["creation_date"])))
-        
-    return formatted_dates
-
-def format_date(data):
-    formatted_date = time.strftime("%d-%m-%Y", time.gmtime(data))
-        
-    return formatted_date
+    app_setup.mongo.db.users.insert_one(user)
     
 def create_recipe(title, description, image, author, servings, favourited, cuisine,
                 category, difficulty, prep_time, cook_time, calories, fat, saturates,
                 carbs, sugars, salt, fibre, protein, allergens, ingredients, instructions):
+    """
+    Creates a recipes object and returns it to be inserted into the database
+    """
+    
     recipe = {
             "title": title,
             "image": image,
@@ -60,91 +57,57 @@ def create_recipe(title, description, image, author, servings, favourited, cuisi
     
     return recipe
     
-def insert_user(username, password):
-    user = {
-        "username": username,
-        "password": password,
-        "favourite_recipes": []
-    }
+def get_top_ten_descending(field):
+    """
+    Returns ten items based on a given field from the database in descending order
+    """
+    return app_setup.mongo.db.recipes.find().sort(field, pymongo.DESCENDING).limit(10)
     
-    app_setup.mongo.db.users.insert_one(user)
+def get_top_ten_ascending(field):
+    """
+    Returns ten items based on a given field from the database in ascending order
+    """
+    return app_setup.mongo.db.recipes.find().sort(field, pymongo.ASCENDING).limit(10)
+    
+def format_dates(data):
+    """
+    Takes in a list of recipes returned from the database and converts
+    their creation date(time in seconds since the epoch) to a string based on the format
+    then adds the strings to a new list and returns it
+    """
+    recipes = data
+    formatted_dates = []
+        
+    for recipe in recipes:
+        formatted_dates.append(time.strftime("%d-%b-%Y", time.gmtime(recipe["creation_date"])))
+        
+    return formatted_dates
 
-def find_unique_items_in_list(list_of_items):
-    list_lowercase = misc.convert_items_in_list_to_lower(list_of_items)
-    list_set = set(list_lowercase)
-    unique_items = list(list_set)
-    
-    return unique_items
-    
-def sort_list(list_to_sort):
-    sorted_list = sorted(list_to_sort)
-    
-    return sorted_list
-    
-def return_capitalized_unique_items_in_list(list_to_search):
-    unique_items = find_unique_items_in_list(list_to_search)
-    sorted_list = sort_list(unique_items)
-    capitalized = misc.convert_items_in_list_to_capitalized(sorted_list)
-    
-    return capitalized
-    
-def list_values(record):
-    list_of_values = []
-    for key, value in record.items():
-        list_of_values.append(value)
+def format_date(data):
+    """
+    Takes in given time in seconds since the epoch and converts it 
+    to a string based on the format and returns it
+    """
+    formatted_date = time.strftime("%d-%b-%Y", time.gmtime(data))
         
-    return list_of_values
+    return formatted_date
     
-def set_filter_title(title_filter):
-    title = None
-    
-    if title_filter != None:
-        title = title_filter 
-    
-    return title
-
-def set_filter_ingredient(ingredient_filter):
-    ingredient = None
-    
-    if ingredient_filter != None:
-        ingredient = ingredient_filter
-        
-    return ingredient
-    
-def set_filter_allergen(allergen_filter):
-    allergen = None
-    
-    if allergen_filter != None:
-        allergen = allergen_filter
-        
-    return allergen
-    
-def set_filter_category(category_filter):
-    category_id = None
-    
-    if category_filter != None:
-        category = app_setup.mongo.db.categories.find_one({"name": category_filter})
-        category_id = category["_id"]
-        
-    return category_id
-    
-def set_filter_cuisine(cuisine_filter):
-    cuisine_id = None
-    
-    if cuisine_filter != None:
-        cuisine = app_setup.mongo.db.cuisines.find_one({"name": cuisine_filter})
-        cuisine_id = cuisine["_id"]
-        
-    return cuisine_id
-
 def assign_value(initial_value, value):
+    """
+    Used mainly when saving request data to a variable which
+    is passed to a url. If value is equal to None returns given
+    value.
+    """
     field = initial_value
     if value != None:
         field = value
     return field
 
-        
 def set_filter_conditions(category_id, cuisine_id, ingredient, allergen, title):
+    """
+    Returns query conditions to be used when querying for recipes to return depending
+    on what filter options where chosen/input by a user.
+    """
     if category_id != None:
         condition_one = {"category": ObjectId(category_id)}
     else:
@@ -156,16 +119,19 @@ def set_filter_conditions(category_id, cuisine_id, ingredient, allergen, title):
         condition_two = {}
 
     if ingredient != "":
+        # Searches for any documents which contain the given word/string in their ingredients, case insensitive
         condition_three = {"ingredients":{"$regex" : ".*" + ingredient + "*", "$options": 'si'}}
     else:
         condition_three = {}
     
     if allergen != None:
+        # Searches for any documents which do not contain the given string/word in their allergens array
         condition_four = {"allergens": {"$nin": [allergen]}}
     else:
         condition_four = {}
         
     if title != "":
+        # Searches for any documents with titles containing the given word/string, case insensitive
         condition_five = {"title":{"$regex" : ".*" + title + "*", "$options": 'si'}}
     else:
         condition_five = {}
@@ -173,9 +139,12 @@ def set_filter_conditions(category_id, cuisine_id, ingredient, allergen, title):
     return (condition_one, condition_two, condition_three, condition_four, condition_five)
     
 def set_sort_condition(sort):
+    """
+    Returns sort condition to be used when querying the database depending on what option a
+    user has selected
+    """
     if sort == "most_popular":
         sort_condition = ("favourited", pymongo.DESCENDING)
-        print(sort_condition)
     elif sort == "least_popular":
         sort_condition = ("favourited", pymongo.ASCENDING)
     elif sort == "oldest": 
@@ -186,6 +155,12 @@ def set_sort_condition(sort):
     return sort_condition
         
 def filtered_recipes(category_id, cuisine_id, ingredient, allergen, title, to_skip, limit, sort):
+    """
+    Main function used for filtering recipes. Takes in users chosen filter and sort options and returns
+    matching recipes. Allows for combining different filter options. Sets how many recipes are
+    returned and how many skipped for pagination purposes. Also returns the number of found recipes.
+    If no filters are set, returns all recipes.
+    """
     conditions = set_filter_conditions(category_id, cuisine_id, ingredient, allergen, title)
     sort_condition = set_sort_condition(sort)
 
@@ -207,6 +182,10 @@ def filtered_recipes(category_id, cuisine_id, ingredient, allergen, title, to_sk
     return (filtered_recipes, filtered_recipes_count)
     
 def return_category_name(category_id):
+    """
+    Searches for a category document with a given id and returns the value of the name
+    field
+    """
     if category_id != None:
         category = app_setup.mongo.db.categories.find_one({"_id": ObjectId(category_id)})
         category_name = category["name"]
@@ -216,6 +195,10 @@ def return_category_name(category_id):
     return category_name
     
 def return_cuisine_name(cuisine_id):
+    """
+    Searches for a cuisine document with a given id and returns the value of the name
+    field
+    """
     if cuisine_id != None:
         cuisine = app_setup.mongo.db.cuisines.find_one({"_id": ObjectId(cuisine_id)})
         cuisine_name = cuisine["name"]
@@ -225,6 +208,9 @@ def return_cuisine_name(cuisine_id):
     return cuisine_name
     
 def return_allergen_name(allergen):
+    """
+    Returns the value if its not equal to None, else returns an empty string
+    """
     if allergen != None:
         allergen_name = allergen
     else:
